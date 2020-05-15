8c477ba3e495f65c4891606e19b305a94c390589
Alexlogvin2019 <alexlogvin@yahoo.com>
2020-05-15 16:28:17 +0300

    ver1

--- contacts.js
+++ contacts.js
@@ -1,72 +1,102 @@
 const fs = require("fs");
 const path = require("path");
+const { promises: fsPromises } = fs;
+const contactsPath = path.join(__dirname, "/db/contacts.json");
 
-/*
-//  * Раскомментируй и запиши значение
- */
-const contactsPath = path.join(__dirname, "./db/contacts.json");
-
-// TODO: задокументировать каждую функцию
-function listContacts() {
-  fs.readFile("./db/contacts.json", "utf-8", function (err, content) {
-    try {
-      const parsedContent = JSON.parse(content);
-      console.table(parsedContent);
-    } catch (error) {
-      console.log("listContacts error", err);
-    }
-  });
+function _getFileData() {
+  return fsPromises
+    .readFile(contactsPath, "utf-8")
+    .then((data) => JSON.parse(data))
+    .catch((err) => err);
 }
-function getContactById(contactId) {
-  fs.readFile("./db/contacts.json", "utf-8", function (err, content) {
-    try {
-      const parsedContent = JSON.parse(content);
-      const findContact = parsedContent.find((item) => item.id === contactId);
-      console.log("findContact", findContact);
-    } catch (error) {
-      console.log("getContactById error", err);
-    }
-  });
+function _randomInteger(min = 0, max = 99) {
+  let rand = min - 0.5 + Math.random() * (max - min + 1);
+  return Math.round(rand);
+}
+function listContacts(req, resp) {
+  _getFileData()
+    .then((data) => {
+      resp.send(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
-function removeContact(contactId) {
-  fs.readFile("./db/contacts.json", "utf-8", function (err, content) {
-    try {
-      const parsedContent = JSON.parse(content);
-      const deleteContact = parsedContent.filter(
-        (item) => item.id != contactId
-      );
-      const newContacts = JSON.stringify(deleteContact);
-      fs.writeFile(contactsPath, newContacts, () => {
-        console.log("remove contact");
+function getContactById({ req, resp, contactId }) {
+  _getFileData()
+    .then((data) => {
+      const contact = data.find((item) => item.id == contactId);
+      if (contact) {
+        resp.send(contact);
+      } else {
+        resp.status(404).send({ message: "Not found" });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+function removeContact({ resp, contactId }) {
+  _getFileData()
+    .then((data) => {
+      const contact = data.find((item) => item.id == contactId);
+      if (!contact) {
+        resp.status(404).send({ message: "Not found" });
+      }
+      const filteredContacts = data.filter((item) => item.id != contactId);
+      fsPromises
+        .writeFile(contactsPath, JSON.stringify(filteredContacts, "", 2))
+        .then(() => {
+          resp.status(200).send({ message: "contact deleted" });
+        });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+function addContact({ name, email, phone, resp }) {
+  _getFileData().then((data) => {
+    const contact = {
+      id: _randomInteger(),
+      name,
+      email,
+      phone,
+    };
+    data.push(contact);
+    fsPromises
+      .writeFile(contactsPath, JSON.stringify(data, "", 2))
+      .then(() => {
+        resp.status(201).send(contact);
+      })
+      .catch((err) => {
+        resp.send(err);
       });
-      console.table(deleteContact);
-    } catch (error) {
-      console.log("removeContact error", err);
-    }
   });
 }
 
-function addContact(name, email, phone) {
-  fs.readFile("./db/contacts.json", "utf-8", function (err, content) {
-    try {
-      const parsedContent = JSON.parse(content);
-      const newContact = { id: 1, name, email, phone };
-      const addContactt = [newContact, ...parsedContent];
-      fs.writeFile(contactsPath, JSON.stringify(addContactt), () => {
-        console.log("Contact added");
-      });
-      console.table(addContactt);
-    } catch (error) {
-      console.log("addContact error", err);
+function updateContact({ req, resp, id }) {
+  _getFileData().then((data) => {
+    const contact = data.findIndex((item) => item.id == id);
+    if (contact == -1) {
+      resp.status(404).send({ message: "Not found" });
+    } else {
+      Object.assign(data[contact], { ...req.body });
+      fsPromises
+        .writeFile(contactsPath, JSON.stringify(data, null, 2))
+        .then(() => {
+          resp.send(data[contact]);
+        });
     }
   });
 }
 
 module.exports = {
-  contactsPath,
   listContacts,
   getContactById,
   removeContact,
   addContact,
+  updateContact,
 };
